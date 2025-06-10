263967e839c76ec80bee35a311286aa3ab5048a5
i18n.host <i18n.host@gmail.com>
2025-06-10 16:28:04 +0800

    v0.1.19

--- sh/dist/meta.sh
+++ sh/dist/meta.sh
@@ -4,4 +4,4 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -e
 
-cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].name,.packages[0].version,.target_directory'
+cargo metadata --no-deps --format-version=1 | jq -br '.packages[0].name,.packages[0].version,.target_directory'
