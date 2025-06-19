7b9d15bb5e187349b7f60678462934d8f7e0f438
i18n.host <i18n.host@gmail.com>
2025-06-19 15:10:46 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![feature(let_chains)]
 
 use aok::{OK, Void};
-use cmdv::cmdv;
 use clap::arg;
 use i18::{Error, tran};
 
@@ -12,19 +11,19 @@ extern "C" fn _loginit() {
 
 #[tokio::main]
 async fn main() -> Void {
-  if let Some((m, _)) = cmdv!(arg!(-w --workdir [path] "workdir"),) {
-    let workdir = m
-      .get_one("workdir")
-      .map(|s: &String| s.into())
-      .unwrap_or_else(|| std::env::current_dir().unwrap_or_else(|_| ".".into()));
-
-    if let Err(e) = tran(&workdir).await {
-      if let Some(e) = e.downcast_ref::<Error>() {
-        eprintln!("{e}");
-        return OK;
-      }
-      return Err(e);
-    }
-  }
-  OK
+  load!(upgrade_host::UPGRADE_HOST,).await
+  // if let Some((m, _)) = cmdv!(arg!(-w --workdir [path] "workdir"),) {
+  //   let workdir = m
+  //     .get_one("workdir")
+  //     .map(|s: &String| s.into())
+  //     .unwrap_or_else(|| std::env::current_dir().unwrap_or_else(|_| ".".into()));
+  //
+  //   if let Err(e) = tran(&workdir).await {
+  //     if let Some(e) = e.downcast_ref::<Error>() {
+  //       eprintln!("{e}");
+  //       return OK;
+  //     }
+  //     return Err(e);
+  //   }
+  // }
 }
